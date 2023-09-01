64d586f169e0be0fcadf2932d83d950a75ab10e5
dnqy <m235208@hiroshima-u.ac.jp>
2023-09-01 11:32:40 +0900

    cjjjj

--- sensor.py
+++ sensor.py
@@ -2,12 +2,8 @@ import time
 import RPi.GPIO as GPIO
 import dht11
 import datetime
-import paho.mqtt.client as mqtt
 
 # configuration
-host = '3.12.43.17' 
-port = 1883 
-topic = 'test' 
 pin = 14
 wait = 1
 
@@ -15,10 +11,6 @@ wait = 1
 GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BCM)
 GPIO.cleanup()
-
-# mqtt initialize
-client = mqtt.Client()
-client.connect(host, port=port)
 
 # read data using pin 14
 instance = dht11.DHT11(pin = pin)
@@ -28,9 +20,9 @@ try:
   while True:
     result = instance.read()
     if result.is_valid():
-      client.publish("Last valid input: " + str(datetime.datetime.now()))
-      client.publish("Temperature: {} C".format(result.temperature))
-      client.publish("Humidity: {} %%".format(result.humidity))
+      print("Last valid input: " + str(datetime.datetime.now()))
+      print("Temperature: {} C".format(result.temperature))
+      print("Humidity: {} %%".format(result.humidity))
     time.sleep(wait)
 except KeyboardInterrupt:
   GPIO.cleanup()
--- sub.py
+++ sub.py
@@ -1,7 +1,7 @@
 import paho.mqtt.client as mqtt
 
 #configuration
-host = 'ec2-3-112-43-17.ap-northeast-1.compute.amazonaws.com'
+host = '3.12.43.17'
 port = 1883
 topic = 'test'
 
